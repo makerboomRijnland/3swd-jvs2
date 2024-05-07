22a9aad21a0f292c0db3c94500082cfb06827b2b
makerboomRijnland <makerboom@mborijnland.nl>
2024-05-07 15:31:39 +0200

    Add .product.locked class. Lock unavailable products

--- corgi-clicker/static/js/cutter.js
+++ corgi-clicker/static/js/cutter.js
@@ -1,26 +1,46 @@
 let corgiCounter = 0;
 let started;
+const products = [
+    { id: "paw", name: "Paw", cost: 10, interval: 10000 },
+    { id: "ancient-corgi", name: "Ancient Corgi", cost: 100, interval: 1000 },
+    { id: "co-kart", name: "Co-Kart", cost: 1000, interval: 100 },
+    { id: "astro-corgi", name: "AstroCorgi", cost: 10000, interval: 10 }
+];
 
 function handleClick() {
-    if(!started) {
+    if (!started) {
         started = new Date();
     }
     corgiCounter++;
-    document.getElementById('cookie-counter').innerHTML = corgiCounter;
+    document.getElementById("cookie-counter").innerHTML = corgiCounter;
+    updateProducts();
 }
 
 function updateClicksPerSecond() {
-    let clicksPerSecond = null;
-    if(started) {
+    let clicksPerSecond = 0;
+    if (started) {
         const time = new Date() - started;
         clicksPerSecond = corgiCounter / (time / 1000);
     }
-    document.getElementById('cookies-per-second').innerHTML = clicksPerSecond.toFixed(1);
+    document.getElementById("cookies-per-second").innerHTML =
+        clicksPerSecond.toFixed(1);
+}
+
+function updateProducts() {
+    for (const product of products) {
+        const element = document.getElementById(product.id);
+        if(corgiCounter < product.cost) {
+            element.classList.add("locked");
+        } else {
+            element.classList.remove("locked");
+        }
+    }
 }
 
 function init() {
-    document.getElementById('cookie').addEventListener('click', handleClick);
+    document.getElementById("cookie").addEventListener("click", handleClick);
     setInterval(updateClicksPerSecond, 100);
+    updateProducts();
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
