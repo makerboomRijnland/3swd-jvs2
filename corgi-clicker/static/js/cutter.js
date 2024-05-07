077ae84dce350d10918cc8d12f79cfd53a1a98e7
makerboomRijnland <makerboom@mborijnland.nl>
2024-05-07 13:43:56 +0200

    Clicks per second

--- corgi-clicker/static/js/cutter.js
+++ corgi-clicker/static/js/cutter.js
@@ -1,8 +1,12 @@
 let corgiCounter = 0;
+let started = new Date();
 
 function handleClick() {
     corgiCounter++;
+    let time = new Date() - started;
+    const clicksPerSecond = corgiCounter / (time / 1000);
     document.getElementById('cookie-counter').innerHTML = corgiCounter;
+    document.getElementById('cookies-per-second').innerHTML = clicksPerSecond.toFixed(1);
 }
 
 function init() {
